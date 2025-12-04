a5ee4ec60bc380710f592dffab0400a1e0dc4e31
Kenneth Sheikh <kenneth.shekikh@hotmail.com>
2025-12-04 18:53:33 +0100

    added robust datastruct NOT NULL

--- schema.sql
+++ schema.sql
@@ -1,13 +1,26 @@
 CREATE TABLE forfatter (
   Forfatter_id INT AUTO_INCREMENT,
-  navn VARCHAR(255),
-  fodselsaar INT,
+  navn VARCHAR(255) NOT NULL,
+  fodselsaar INT NOT NULL,
   PRIMARY KEY (Forfatter_id)
 );
 
 CREATE TABLE forlag (
     forlag_id INT AUTO_INCREMENT,
-    navn VARCHAR(255),
-    adresse VARCHAR(255),
+    navn VARCHAR(255) NOT NULL,
+    adresse VARCHAR(255) NOT NULL,
     PRIMARY KEY (forlag_id)
 );
+
+CREATE TABLE bok (
+    isbn VARCHAR(20) NOT NULL,
+    tittel VARCHAR(255) NOT NULL,
+    utgivelsesaar INT NOT NULL,
+    antall_sider INT NOT NULL,
+    Forfatter_id INT NOT NULL,
+    forlag_id INT NOT NULL,
+    PRIMARY KEY (isbn),
+    FOREIGN KEY (Forfatter_id) REFERENCES forfatter(Forfatter_id),
+    FOREIGN KEY (forlag_id) REFERENCES forlag(forlag_id)
+);
+
